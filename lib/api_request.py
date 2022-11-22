76e0ed7cca3d4459c8450e9ddf68cdabc76c5a42
Ruben Melo <rubenmelo02@gmail.com>
2022-11-22 19:35:41 -0300

    Add new api request feature

--- lib/api_request.py
+++ lib/api_request.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_quote(currency):
+def get_quote(currency:str):
 
     if currency == 'usd':
         return requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL').json()['USDBRL']['bid'][:4].replace('.',',')
@@ -13,5 +13,11 @@ def get_quote(currency):
         return requests.get('https://economia.awesomeapi.com.br/json/last/BTC-BRL').json()['BTCBRL']['bid']
 
 
-def get_address(cep):
-    pass
+def get_address(cep:str):
+    
+    try:
+        ads = requests.get(f'https://cdn.apicep.com/file/apicep/{cep[:5]}-{cep[5:]}.json').json()
+        return True, f'\nCode: {ads["code"]}\nState: {ads["state"]}\nCity: {ads["city"]}\nDistrict: {ads["district"]}\nAddress: {ads["address"]}'
+
+    except:
+        return False, '\nAn error occurred. Try another CEP.'
--- lib/scripts.py
+++ lib/scripts.py
@@ -1,17 +1,17 @@
 import os
 from time import sleep
-from lib.api_request import get_quote
+from lib.api_request import get_quote, get_address
 
 
 def main_options():
     os.system('cls')
-    print(open("lib\\title.txt", "r").read())
+    print(open('lib\\title.txt', 'r').read())
     print(f'\n\n{"Choose an option":^48}\n\n1- Currency quote\n2- Address by zip code\n3- Exit')
     c = 0
     while True:
         op = input('> ')
         if op == '1': currency_quote(); break
-        elif op == '2': main_options(); break
+        elif op == '2': address_detail(); break
         elif op == '3': close_app()
         else:
             if c == 3: main_options(); break
@@ -19,14 +19,25 @@ def main_options():
 
 
 def currency_quote():
-    print(f"\n{'downloading data...':^48}")
-    usd, eur, btc = get_quote("usd"), get_quote("eur"), get_quote("btc")
+    print(f'\n{"downloading data...":^48}')
+    usd, eur, btc = get_quote('usd'), get_quote('eur'), get_quote('btc')
     os.system('cls')
     print(f'\n\tDOLLAR\t\tR$ {usd}\n\tEURO\t\tR$ {eur}\n\tBITCOIN\t\tR$ {btc}')
-    sleep(1.5)
+
+
+def address_detail():
+    cep = input('\nCEP: ')
+    print(f'\n{"please wait...":^48}')
+    shot = get_address(cep)
+    if shot[0]:
+        os.system('cls')
+        print(shot[1])
+    else:
+        print(shot[1])
 
 
 def end_options():
+    sleep(1.5)
     print('\n\n1- Home\n2- Exit')
     c = 0
     while True:
@@ -35,7 +46,7 @@ def end_options():
         elif choice == '2': close_app()
         else:
             if c == 3:
-                print(f"\n{'redirecting to Home...':^48}")
+                print(f'\n{"redirecting to Home...":^48}')
                 sleep(1.1)
                 break
             print('\nChoose a valid option.')
@@ -43,6 +54,6 @@ def end_options():
 
 
 def close_app():
-    print(f"\n{'closing app...':^48}")
+    print(f'\n{"closing app...":^48}')
     sleep(1.3)
     exit()
